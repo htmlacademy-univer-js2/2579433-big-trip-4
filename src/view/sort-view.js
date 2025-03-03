4b796616472d14803414e26c3d66cc74db067cf9
Im60duck <kvashnin.andruha@yandex.ru>
2025-03-03 23:31:57 +0700

    data to parent

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -4,9 +4,9 @@ import { capitalizeFirstLetter } from '../utils.js';
 
 function createSortTemplate(sortType) {
   const sortList = SORT_TYPES.map((type) =>
-    `<div class="trip-sort__item  trip-sort__item--${type}">
+    `<div class="trip-sort__item  trip-sort__item--${type}" data-sort-type = ${type}>
         <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${type === sortType ? 'checked' : ''} ${type === SORT_TYPES[1] || type === SORT_TYPES[4] ? 'disabled' : ''}>
-        <label class="trip-sort__btn" data-sort-type = ${type} for="sort-${type}">${capitalizeFirstLetter(type)}</label>
+        <label class="trip-sort__btn" for="sort-${type}">${capitalizeFirstLetter(type)}</label>
     </div>`
   ).join('\n');
 
@@ -30,7 +30,7 @@ export default class SortView extends AbstractView{
     this.element.querySelector(`.trip-sort__item--${SORT_TYPES[3]}`).addEventListener('click', this.#handleSortTypeChange);
   }
 
-  #handleSortTypeChange = (evt) => this.#onSortTypeChange(evt.target.dataset.sortType);
+  #handleSortTypeChange = (evt) => this.#onSortTypeChange(evt.currentTarget.dataset.sortType);
 
   get template() {
     return createSortTemplate(this.#sortType);
