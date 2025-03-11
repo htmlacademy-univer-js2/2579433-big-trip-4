33b99d3724970f98015c57db8953264e3c6c0951
Im60duck <kvashnin.andruha@yandex.ru>
2025-03-12 01:11:59 +0700

    bulky trip info

--- src/presenter/trip-info-presenter.js
+++ src/presenter/trip-info-presenter.js
@@ -1,4 +1,4 @@
-import {render, replace, remove} from '../framework/render.js';
+import {render, replace, remove, RenderPosition} from '../framework/render.js';
 import TripInfoView from '../view/trip-info-view.js';
 
 export default class TripInfoPresenter {
@@ -13,25 +13,62 @@ export default class TripInfoPresenter {
     this.#pointModel.addObserver(this.#handleModelEvent);
   }
 
-  // get startPoint(){
-  //   const startDate = Math.min();
-  // }
+  get startPoint(){
+    const result = this.#pointModel.points.reduce((earliest, current) =>
+      current.dateFrom < earliest.dateFrom ? current : earliest, this.#pointModel.points[0]);
 
-  // get middlePoint(){}
+    return {dateFrom: result.dateFrom, destination: result.destination};
+  }
 
-  // get finishPoint(){}
+  get finishPoint(){
+    const result = this.#pointModel.points.reduce((latest, current) =>
+      current.dateTo > latest.dateTo ? current : latest, this.#pointModel.points[0]);
 
-  // get finalPrice(){}
+    return {dateTo: result.dateTo, destination: result.destination};
+  }
+
+  get finalPrice(){
+    const totalBasePrice = this.#pointModel.points.reduce((sum, point) => sum + parseInt(point.basePrice, 10), 0);
+
+    const offersMap = new Map();
+    this.#pointModel.offers.forEach(({type, offers}) => {
+      offers.forEach((offer) => {
+        offersMap.set(`${type}-${offer.id}`, offer.price);
+      });
+    });
+
+    const totalOffersPrice = this.#pointModel.points.reduce((sum, point) => {
+      const pointOffersPrice = point.offers.reduce((offerSum, offerId) => {
+        const key = `${point.type}-${offerId}`;
+        return offerSum + (parseInt(offersMap.get(key), 10) || 0);
+      }, 0);
+      return sum + pointOffersPrice;
+    }, 0);
+
+    return totalBasePrice + totalOffersPrice;
+  }
+
+  get filteredDestinations(){
+    const pointsDestinations = [...new Set(this.#pointModel.points.map((point) => point.destination))];
+    return this.#pointModel.destinations.filter((d) => pointsDestinations.includes(d.id));
+  }
 
   init(){
+    if(this.#pointModel.points.length === 0){
+      remove(this.#infoComponent);
+      return;
+    }
     const prevInfoComponent = this.#infoComponent;
 
     this.#infoComponent = new TripInfoView({
-
+      startPoint: this.startPoint,
+      finishPoint: this.finishPoint,
+      finalPrice: this.finalPrice,
+      destinations: this.filteredDestinations
     });
 
     if (prevInfoComponent === null) {
-      render(this.#infoComponent, this.#infoContainer);
+      render(this.#infoComponent, this.#infoContainer, RenderPosition.AFTERBEGIN);
     }else{
       replace(this.#infoComponent, prevInfoComponent);
       remove(prevInfoComponent);
